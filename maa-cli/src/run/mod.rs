a4eacedd2e611bb9ac66a6ad5704a43dcb07c8b5
Long Wang <wangl.cc@outlook.com>
2023-09-01 15:59:38 +0800

    feat: load resources based on `cilent_type` and if use `PlayTools`

--- maa-cli/src/run/mod.rs
+++ maa-cli/src/run/mod.rs
@@ -41,9 +41,9 @@ pub fn run(
     debug!("State directory:", state_dir.display());
     Assistant::set_user_dir(state_dir).context("Failed to set user directory!")?;
 
-    let data_dir = find_resource(dirs).context("Failed to find resource!")?;
-    debug!("Data directory:", data_dir.display());
-    Assistant::load_resource(data_dir.parent().unwrap()).context("Failed to load resource!")?;
+    let resource_dir = find_resource(dirs).context("Failed to find resource!")?;
+    debug!("Resources directory:", resource_dir.display());
+    Assistant::load_resource(resource_dir.parent().unwrap()).context("Failed to load resource!")?;
 
     /*--------------------- Load Config Files ---------------------*/
     let config_dir = dirs.config();
@@ -72,35 +72,34 @@ pub fn run(
         )
     })?;
 
-    /*------------------ Additional resource files -----------------*/
-    for resource in asst_config.resources.iter() {
-        let path = PathBuf::from(resource);
-        let path = if path.is_absolute() {
-            debug!("Loading additional resource:", path.display());
-            path
-        } else {
-            debug!("Loading additional resource:", resource);
-            data_dir.join(resource)
-        };
-        Assistant::load_resource(&path)
-            .with_context(|| format!("Failed to load additional resource {}!", path.display()))?;
-    }
-
-    if user_resource {
-        if config_dir.join("resource").exists() {
-            debug!("Loading user resource:", config_dir.display());
-            Assistant::load_resource(config_dir).context("Failed to load user resource!")?;
-        } else {
-            warning!("`--user-resource` is specified, but no user resource found!");
+    /*--------------------- Process Connection ---------------------*/
+    let mut playtools: bool = false;
+    let (adb_path, address, config) = match asst_config.connection {
+        Connection::ADB {
+            adb_path,
+            device,
+            config,
+        } => {
+            debug!("Setting adb_path to", &adb_path);
+            debug!("Setting device to", &device);
+            debug!("Setting config to", &config);
+            (adb_path, device, config)
         }
-    }
+        Connection::PlayTools { address, config } => {
+            playtools = true;
+            let address = addr.unwrap_or(address);
+            debug!("Setting address to", &address);
+            debug!("Setting config to", &config);
+            (String::new(), address, config)
+        }
+    };
 
-    /*----------------------- Process Task --------------------------*/
+    /*----------------------- Process Task -------------------------*/
     let mut task_typs: Vec<TaskType> = Vec::new();
     let mut task_params: Vec<String> = Vec::new();
-    let mut start_app: u8 = 0;
-    let mut close_app: u8 = 0;
-    let mut app_name: &str = "";
+    let mut start_app: bool = false; // start iOS app before connect
+    let mut close_app: bool = false; // close iOS app after disconnect
+    let mut app = PlayCoverApp::new("明日方舟");
 
     for task in task_list.tasks {
         if task.is_active() {
@@ -115,32 +114,68 @@ pub fn run(
                         }
                         None => true,
                     };
-                    let client_type = match params.get("client_type") {
-                        Some(client_type) => client_type
-                            .as_str()
-                            .ok_or(anyhow!("key client_type must be string"))?,
-                        None => "",
+                    match params.get("client_type") {
+                        Some(client_type) => {
+                            let client_type = client_type
+                                .as_str()
+                                .ok_or(anyhow!("key client_type must be string"))?;
+
+                            match client_type {
+                                "Official" | "Bilibili" | "" => (),
+                                "txwy" => {
+                                    debug!("Loading additional resource", "for txwy");
+                                    let resource_path = resource_dir.join("global").join("txwy");
+                                    Assistant::load_resource(&resource_path)
+                                        .context("Failed to load additional resource!")?;
+                                }
+                                "YoStarEN" => {
+                                    debug!("Loading additional resource", "for YoStarEN");
+                                    let resource_path =
+                                        resource_dir.join("global").join("YoStarEN");
+                                    Assistant::load_resource(&resource_path)
+                                        .context("Failed to load additional resource!")?;
+                                    app = PlayCoverApp::new("Arknights");
+                                }
+                                "YoStarJP" => {
+                                    debug!("Loading additional resource", "for YoStarJP");
+                                    let resource_path =
+                                        resource_dir.join("global").join("YoStarJP");
+                                    Assistant::load_resource(&resource_path)
+                                        .context("Failed to load additional resource!")?;
+                                    app = PlayCoverApp::new("アークナイツ");
+                                }
+                                "YoStarKR" => {
+                                    debug!("Loading additional resource", "for YoStarKR");
+                                    let resource_path =
+                                        resource_dir.join("global").join("YoStarKR");
+                                    Assistant::load_resource(&resource_path)
+                                        .context("Failed to load additional resource!")?;
+                                    app = PlayCoverApp::new("명일방주");
+                                }
+                                _ => {
+                                    warning!(
+                                        format!("Unknown client type \"{}\",", client_type),
+                                        "using default app name \"明日方舟\""
+                                    );
+                                }
+                            };
+                        }
+                        None => {
+                            if playtools {
+                                warning!(
+                                    "No client type specified",
+                                    "using default app name \"明日方舟\""
+                                );
+                            }
+                        }
                     };
                     let start_game = params
                         .get("start_game_enabled")
                         .unwrap_or(&Value::Bool(false))
                         .as_bool()
                         .ok_or(anyhow!("key enable must be bool"))?;
-                    if enable && start_game {
-                        start_app += 1;
-                        app_name = match client_type {
-                            "Official" | "Bilibili" | "txwy" | "" => "明日方舟",
-                            "YoStarEN" => "Arknights",
-                            "YoStarJP" => "アークナイツ",
-                            "YoStarKR" => "명일방주",
-                            _ => {
-                                warning!(
-                                    format!("Unknown client type: {}", client_type),
-                                    "using default name: 明日方舟"
-                                );
-                                "明日方舟"
-                            }
-                        };
+                    if playtools && enable && start_game {
+                        start_app = true;
                     }
                 }
                 TaskType::CloseDown => {
@@ -150,9 +185,7 @@ pub fn run(
                         }
                         None => true,
                     };
-                    if enable {
-                        close_app += 1;
-                    }
+                    close_app = playtools && enable;
                 }
                 _ => {
                     // For any task that has a filename parameter
@@ -188,6 +221,36 @@ pub fn run(
         }
     }
 
+    /*------------------- Load Additional resource -----------------*/
+    if playtools {
+        debug!("Load additional resource", "for PlayTools");
+        Assistant::load_resource(resource_dir.join("platform_diff/iOS"))
+            .context("Failed to load additional resource!")?;
+    }
+
+    for resource in asst_config.resources.iter() {
+        let path = PathBuf::from(resource);
+        let path = if path.is_absolute() {
+            debug!("Loading additional resource:", path.display());
+            path
+        } else {
+            debug!("Loading additional resource:", resource);
+            resource_dir.join(resource)
+        };
+        Assistant::load_resource(&path)
+            .with_context(|| format!("Failed to load additional resource {}!", path.display()))?;
+    }
+
+    if user_resource {
+        if config_dir.join("resource").exists() {
+            debug!("Loading user resource:", config_dir.display());
+            Assistant::load_resource(config_dir).context("Failed to load user resource!")?;
+        } else {
+            warning!("`--user-resource` is specified, but no user resource found!");
+        }
+    }
+
+    /*------------------------ Init Assistant ----------------------*/
     let stop_bool = Arc::new(std::sync::atomic::AtomicBool::new(false));
     for sig in TERM_SIGNALS {
         signal_hook::flag::register_conditional_default(*sig, Arc::clone(&stop_bool))
@@ -195,16 +258,38 @@ pub fn run(
         signal_hook::flag::register(*sig, Arc::clone(&stop_bool))
             .context("Failed to register signal handler!")?;
     }
-
-    /* ----------------------- Init Assistant ----------------------*/
     let assistant = Assistant::new(Some(callback), None);
 
-    /* ----------------------- Setup Instance ----------------------*/
+    /*------------------------ Setup Instance ----------------------*/
     let options = asst_config.instance_options;
     if let Some(v) = options.touch_mode {
-        debug!("Setting touch_mode to", v);
+        if playtools && v != asst::TouchMode::MacPlayTools {
+            warning!(
+                "Wrong touch mode,",
+                "force set touch_mode to MacPlayTools when using PlayTools"
+            );
+            assistant
+                .set_instance_option(2, asst::TouchMode::MacPlayTools)
+                .context("Failed to set touch mode!")?;
+        } else {
+            debug!("Setting touch_mode to", v);
+            assistant
+                .set_instance_option(2, v)
+                .context("Failed to set touch mode!")?;
+        }
+    } else if playtools {
+        debug!("Setting touch_mode to MacPlayTools");
         assistant
-            .set_instance_option(2, v)
+            .set_instance_option(2, asst::TouchMode::MacPlayTools)
+            .context("Failed to set touch mode!")?;
+    } else {
+        let mode = asst::TouchMode::default();
+        warning!(
+            "No touch mode specified,",
+            format!("using default touch mode {}.", mode)
+        );
+        assistant
+            .set_instance_option(2, mode)
             .context("Failed to set touch mode!")?;
     }
     if let Some(v) = options.deployment_with_pause {
@@ -223,76 +308,11 @@ pub fn run(
     }
 
     /*----------------------- Connect to Game ----------------------*/
-    let connection = asst_config.connection;
-    match connection {
-        Connection::ADB {
-            adb_path,
-            device,
-            config,
-        } => {
-            if options.touch_mode.is_none() {
-                warning!(
-                    "No touch mode specified, set to",
-                    asst::TouchMode::default()
-                );
-                assistant
-                    .set_instance_option(2, asst::TouchMode::default())
-                    .context("Failed to set touch mode!")?;
-            }
-
-            debug!("Setting adb_path to", &adb_path);
-            debug!("Setting device to", &device);
-            debug!("Setting config to", &config);
-            let adb_device = addr.unwrap_or(device);
-            assistant.async_connect(adb_path, adb_device, config, true)?;
-        }
-        Connection::PlayTools { address, config } => {
-            let address = addr.unwrap_or(address);
-            debug!("Setting address to", &address);
-            debug!("Setting config to", &config);
-
-            // NOTE:
-            // If the game is launched from terminal,
-            // there are some connection issues with server.
-            // Even launching the game from another app
-            // which can launch the game successfully,
-            // the connection issues still exist.
-            // I'm not sure if this is a bug of PlayCover or macOS.
-            // But it seems not bug of maa-cli
-            if start_app > 0 {
-                normal!("Starting game...");
-                std::process::Command::new("open")
-                    .arg("-a")
-                    .arg(app_name)
-                    .spawn()
-                    .context("Failed to start game!")?
-                    .wait()
-                    .context("Failed to start game!")?;
-            }
-            close_app += 1;
-
-            if let Some(v) = options.touch_mode {
-                if v != asst::TouchMode::MacPlayTools {
-                    warning!(
-                        "Wrong touch mode,",
-                        "force set touch_mode to MacPlayTools when using PlayTools"
-                    );
-                    assistant
-                        .set_instance_option(2, asst::TouchMode::MacPlayTools)
-                        .context("Failed to set touch mode!")?;
-                }
-            } else {
-                assistant
-                    .set_instance_option(2, asst::TouchMode::MacPlayTools)
-                    .context("Failed to set touch mode!")?;
-            }
-
-            // Wait for the game to start
-            std::thread::sleep(std::time::Duration::from_secs(5));
-
-            assistant.async_connect("", address, config, true)?;
-        }
+    if start_app {
+        app.open()?;
+        std::thread::sleep(std::time::Duration::from_secs(5));
     }
+    assistant.async_connect(adb_path, address, config, true)?;
 
     /* ------------------------- Append Tasks ----------------------*/
     for (i, task_type) in task_typs.iter().enumerate() {
@@ -313,23 +333,8 @@ pub fn run(
     stop_bool.store(true, std::sync::atomic::Ordering::Relaxed);
 
     /* ------------------------- Close Game ------------------------*/
-    if close_app > 1 {
-        let app_name = match app_name {
-            "" => {
-                warning!(
-                    "No app name specified",
-                    "using default name: 明日方舟, please specify app name in startup task"
-                );
-                "明日方舟"
-            }
-            _ => app_name,
-        };
-        normal!("Closing game...");
-        std::process::Command::new("osascript")
-            .arg("-e")
-            .arg(format!("quit app \"{}\"", app_name))
-            .status()
-            .context("Failed to close game!")?;
+    if close_app {
+        app.close();
     }
 
     Ok(())
@@ -341,4 +346,41 @@ pub fn core_version<'a>(dirs: &Dirs) -> Result<&'a str> {
     maa_sys::binding::load(core_path);
 
     Ok(Assistant::get_version()?)
+}
+
+struct PlayCoverApp<'a> {
+    name: &'a str,
+}
+
+impl<'a> PlayCoverApp<'a> {
+    pub fn new(name: &'a str) -> Self {
+        Self { name }
+    }
+
+    pub fn open(&self) -> Result<()> {
+        // NOTE:
+        // If the game is launched from terminal,
+        // there are some connection issues with server.
+        // Even launching the game from another app
+        // which can launch the game successfully,
+        // the connection issues still exist.
+        // I'm not sure if this is a bug of PlayCover or macOS.
+        // But it seems not bug of maa-cli
+        normal!("Starting game...");
+        std::process::Command::new("open")
+            .arg("-a")
+            .arg(self.name)
+            .status()
+            .context("Failed to start game!")?;
+        Ok(())
+    }
+
+    pub fn close(&self) {
+        normal!("Closing game...");
+        std::process::Command::new("osascript")
+            .arg("-e")
+            .arg(format!("quit app \"{}\"", self.name))
+            .status()
+            .expect("Failed to close game!");
+    }
 }
