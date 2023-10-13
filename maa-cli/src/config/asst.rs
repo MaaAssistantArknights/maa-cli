691ac30cbd1ba4ddcfebf322b8d44b23b31cac8d
Loong <wangl.cc@outlook.com>
2023-10-13 17:35:33 +0800

    refactor: use `AsRef<str>` instead of `From` to convert `TouchMode`

--- maa-cli/src/config/asst.rs
+++ maa-cli/src/config/asst.rs
@@ -33,9 +33,9 @@ pub enum TouchMode {
     MacPlayTools,
 }
 
-impl<'a> From<TouchMode> for &'a str {
-    fn from(mode: TouchMode) -> Self {
-        match mode {
+impl AsRef<str> for TouchMode {
+    fn as_ref(&self) -> &str {
+        match self {
             TouchMode::ADB => "adb",
             TouchMode::MiniTouch => "minitouch",
             TouchMode::MAATouch => "maatouch",
@@ -46,13 +46,13 @@ impl<'a> From<TouchMode> for &'a str {
 
 impl std::fmt::Display for TouchMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", <&str>::from(*self))
+        write!(f, "{}", self.as_ref())
     }
 }
 
 impl maa_sys::ToCString for TouchMode {
     fn to_cstring(self) -> maa_sys::Result<std::ffi::CString> {
-        <&str>::from(self).to_cstring()
+        self.as_ref().to_cstring()
     }
 }
 
@@ -117,57 +117,89 @@ impl super::FromFile for AsstConfig {}
 mod tests {
     use super::*;
 
-    #[test]
-    fn deserialize_example() {
-        let config: AsstConfig =
-            toml::from_str(&std::fs::read_to_string("../config_examples/asst.toml").unwrap())
-                .unwrap();
-        assert_eq!(
-            config,
-            AsstConfig {
-                user_resource: true,
-                resources: vec![String::from("platform_diff/macOS")],
-                connection: Connection::ADB {
-                    adb_path: String::from("adb"),
-                    device: String::from("emulator-5554"),
-                    config: String::from("CompatMac"),
-                },
-                instance_options: InstanceOptions {
-                    touch_mode: Some(TouchMode::MAATouch),
-                    deployment_with_pause: Some(false),
-                    adb_lite_enabled: Some(false),
-                    kill_adb_on_exit: Some(false),
-                },
-            }
-        );
+    mod touch_mode {
+        use super::*;
+
+        use std::ffi::CString;
+
+        use maa_sys::ToCString;
+
+        #[test]
+        fn to_cstring() {
+            assert_eq!(
+                TouchMode::ADB.to_cstring().unwrap(),
+                CString::new("adb").unwrap()
+            );
+            assert_eq!(
+                TouchMode::MiniTouch.to_cstring().unwrap(),
+                CString::new("minitouch").unwrap()
+            );
+            assert_eq!(
+                TouchMode::MAATouch.to_cstring().unwrap(),
+                CString::new("maatouch").unwrap()
+            );
+            assert_eq!(
+                TouchMode::MacPlayTools.to_cstring().unwrap(),
+                CString::new("MacPlayTools").unwrap()
+            );
+        }
     }
 
-    #[test]
-    fn deserialize_empty() {
-        let config: AsstConfig = toml::from_str("").unwrap();
-        assert_eq!(
-            config,
-            AsstConfig {
-                user_resource: false,
-                resources: vec![],
-                connection: Connection::ADB {
-                    adb_path: String::from("adb"),
-                    device: String::from("emulator-5554"),
-                    config: if cfg!(target_os = "macos") {
-                        String::from("CompatMac")
-                    } else if cfg!(target_os = "linux") {
-                        String::from("CompatPOSIXShell")
-                    } else {
-                        String::from("General")
+    mod serde {
+        use super::*;
+
+        #[test]
+        fn deserialize_example() {
+            let config: AsstConfig =
+                toml::from_str(&std::fs::read_to_string("../config_examples/asst.toml").unwrap())
+                    .unwrap();
+            assert_eq!(
+                config,
+                AsstConfig {
+                    user_resource: true,
+                    resources: vec![String::from("platform_diff/iOS")],
+                    connection: Connection::ADB {
+                        adb_path: String::from("adb"),
+                        device: String::from("emulator-5554"),
+                        config: String::from("CompatMac"),
                     },
-                },
-                instance_options: InstanceOptions {
-                    touch_mode: None,
-                    deployment_with_pause: None,
-                    adb_lite_enabled: None,
-                    kill_adb_on_exit: None,
-                },
-            }
-        );
+                    instance_options: InstanceOptions {
+                        touch_mode: Some(TouchMode::MAATouch),
+                        deployment_with_pause: Some(false),
+                        adb_lite_enabled: Some(false),
+                        kill_adb_on_exit: Some(false),
+                    },
+                }
+            );
+        }
+
+        #[test]
+        fn deserialize_empty() {
+            let config: AsstConfig = toml::from_str("").unwrap();
+            assert_eq!(
+                config,
+                AsstConfig {
+                    user_resource: false,
+                    resources: vec![],
+                    connection: Connection::ADB {
+                        adb_path: String::from("adb"),
+                        device: String::from("emulator-5554"),
+                        config: if cfg!(target_os = "macos") {
+                            String::from("CompatMac")
+                        } else if cfg!(target_os = "linux") {
+                            String::from("CompatPOSIXShell")
+                        } else {
+                            String::from("General")
+                        },
+                    },
+                    instance_options: InstanceOptions {
+                        touch_mode: None,
+                        deployment_with_pause: None,
+                        adb_lite_enabled: None,
+                        kill_adb_on_exit: None,
+                    },
+                }
+            );
+        }
     }
 }
