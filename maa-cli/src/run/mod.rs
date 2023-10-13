052cd067e4d9143e203886df1ad52b1b8092f3d6
Loong <wangl.cc@outlook.com>
2023-10-14 02:47:58 +0800

    fix dependencies

--- maa-cli/src/run/mod.rs
+++ maa-cli/src/run/mod.rs
@@ -463,7 +463,7 @@ fn load_core(dirs: &Dirs) {
         #[cfg(target_os = "windows")]
         {
             use std::os::windows::ffi::OsStrExt;
-            use windows_sys::windows::Win32::System::LibraryLoader::SetDllDirectoryW;
+            use windows_sys::Win32::System::LibraryLoader::SetDllDirectoryW;
 
             let lib_dir_w: Vec<u16> = lib_dir.as_ref().encode_wide().chain(Some(0)).collect();
             unsafe { SetDllDirectoryW(lib_dir_w.as_ptr()) };
