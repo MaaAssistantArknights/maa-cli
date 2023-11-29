529b28e676bada3b30c5de6b211f22929a69cb09
Loong <wangl.cc@outlook.com>
2023-11-29 22:37:16 +0800

    ci: only update version info when publishing stable release and allow to manually trigger alpha release

--- .github/scripts/parse_version.sh
+++ .github/scripts/parse_version.sh
@@ -2,20 +2,12 @@
 
 set -e
 
-CARGO_PKG_VERSION=$(yq -r '.package.version' maa-cli/Cargo.toml)
+CARGO_PKG_VERSION=$(yq -oy -r '.package.version' maa-cli/Cargo.toml)
 commit_sha=$(git rev-parse HEAD)
 
-if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
-  echo "PR detected, marking version as alpha pre-release and skipping publish"
+release_alpha() {
   channel="alpha"
-  version="$CARGO_PKG_VERSION-alpha.$(date +%s)"
-  tag="nightly"
-  publish="false"
-elif [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
-  echo "Scheduled event detected, marking version as alpha pre-release and publish to alpha channel"
-  # check if there are some new commits
-  channel="alpha"
-  published_commit=$(yq -r ".details.commit" version/$channel.json)
+  published_commit=$(yq -oy -r ".details.commit" version/$channel.json)
   if [ "$published_commit" == "$commit_sha" ]; then
     echo "No new commits, exiting, skipping all steps"
     echo "skip=true" >> "$GITHUB_OUTPUT"
@@ -23,47 +15,73 @@ elif [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
   fi
   version="$CARGO_PKG_VERSION-alpha.$(date +%s)"
   tag="nightly"
+}
+
+release_beta() {
+  channel="beta"
+  published_version=$(yq -oy -r ".details.version" version/$channel.json)
+  published_version_prefix=${published_version%-*}
+  published_version_suffix=${published_version#*-}
+  if [ "$published_version_prefix" != "$CARGO_PKG_VERSION" ]; then
+    echo "Last published version is not the same as current version (published: $published_version)"
+    version="$CARGO_PKG_VERSION-$channel.1"
+  elif [ "$published_version_suffix" == "$published_version" ]; then
+    echo "Last published version is not a pre-release version (published: $published_version)"
+    version="$CARGO_PKG_VERSION-$channel.1"
+  else
+    beta_number=${published_version_suffix#*.}
+    version="$CARGO_PKG_VERSION-$channel.$((beta_number + 1))"
+  fi
+}
+
+if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+  echo "PR detected, marking version as alpha pre-release and skipping publish"
+  release_alpha
+  publish="false"
+elif [ "$GITHUB_EVENT_NAME" == "schedule" ]; then
+  echo "Scheduled event detected, marking version as alpha pre-release and publish to alpha channel"
+  release_alpha
+  tag="nightly"
 elif [ "$GITHUB_EVENT_NAME" == "workflow_dispatch" ]; then
   echo "Workflow dispatch event detected, reading inputs"
-  beta=$(yq -r '.inputs.beta' "$GITHUB_EVENT_PATH")
-  if [ "$beta" == "true" ]; then
-    echo "Beta flag detected, marking version as beta pre-release and publish to beta channel"
-    channel="beta"
-    published_version=$(yq -r ".details.version" version/beta.json)
-    published_version_prefix=${published_version%-*}
-    published_version_suffix=${published_version#*-}
-    if [ "$published_version_prefix" != "$CARGO_PKG_VERSION" ]; then
-      echo "Last published version is not the same as current version (published: $published_version)"
-      version="$CARGO_PKG_VERSION-$channel.1"
-    elif [ "$published_version_suffix" == "$published_version" ]; then
-      echo "Last published version is not a pre-release version (published: $published_version)"
-      version="$CARGO_PKG_VERSION-$channel.1"
-    else
-      beta_number=${published_version_suffix#*.}
-      version="$CARGO_PKG_VERSION-$channel.$((beta_number + 1))"
-    fi
+  channel=$(yq -oy -r '.inputs.channel' "$GITHUB_EVENT_PATH")
+  if [ "$channel" == "alpha" ]; then
+    echo "Dispatched alpha channel, marking version as alpha pre-release"
+    release_alpha
+    tag="nightly"
+  elif [ "$channel" == "beta" ]; then
+    echo "Beta channel detected, marking version as beta pre-release and publish to beta channel"
+    release_beta
+  elif [ "$channel" == "stable" ]; then
+    echo "Stable channel detected, marking version as stable release and publish to stable channel"
   else
-    echo "No beta flag detected, marking version as stable release and publish to stable channel"
-    channel="stable"
+    echo "Unknown channel $channel, aborting"
+    exit 1
   fi
-  publish=$(yq -r '.inputs.publish' "$GITHUB_EVENT_PATH")
-else # push to a tag
-  REF_VERSION=${GITHUB_REF#refs/tags/v}
-  if [ "$REF_VERSION" == "$GITHUB_REF" ]; then
+  publish=$(yq -oy -r '.inputs.publish' "$GITHUB_EVENT_PATH")
+elif [ "$GITHUB_EVENT_NAME" == "push" ]; then
+  ref_version=${GITHUB_REF#refs/tags/v}
+  if [ "$ref_version" == "$GITHUB_REF" ]; then
     echo "Version tag not matched, aborting"
     exit 1
   fi
   echo "Tag detected, marking version as stable release and publish to stable channel"
-  channel="stable"
-  version="$REF_VERSION"
+else
+  echo "Unknown event $GITHUB_EVENT_NAME, aborting"
+  exit 1
 fi
 
-echo "Release version $version to $channel channel with tag $tag"
+channel=${channel:-stable}
+version=${version:-$CARGO_PKG_VERSION}
+tag=${tag:-v$version}
+publish=${publish:-true}
+
+echo "Release version $version with tag $tag to channel $channel (publish: $publish)"
 {
   echo "commit=$commit_sha"
   echo "channel=$channel"
   echo "version=$version"
-  echo "tag=${tag:-v$version}"
-  echo "publish=${publish:-ture}"
+  echo "tag=$tag"
+  echo "publish=$publish"
   echo "skip=false"
 } >> "$GITHUB_OUTPUT"
