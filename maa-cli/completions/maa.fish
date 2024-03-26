c0eac53908781e436f3afe7ffd4328a243f98eca
Loong <wangl.cc@outlook.com>
2024-03-26 13:03:25 +0800

    chore: add fish completion for cleanup and mangen (#238)

--- maa-cli/completions/maa.fish
+++ maa-cli/completions/maa.fish
@@ -33,7 +33,9 @@ __maa_add_subcommand activity 'Show stage activity of given client'
 __maa_add_subcommand remainder 'Get remainder of given divisor and current date'
 __maa_add_subcommand list 'List all available tasks'
 __maa_add_subcommand complete 'Generate completion script for given shell'
+__maa_add_subcommand mangen 'Generate man page for maa-cli at given path'
 __maa_add_subcommand convert 'Convert config file to another format'
+__maa_add_subcommand cleanup 'Cleanup maa-cli and MaaCore cache'
 
 # options for subcommands
 # help
@@ -80,6 +82,8 @@ complete -c maa -n "__fish_seen_subcommand_from roguelike" -a "phantom mizuki sa
 
 # Misc commands
 complete -c maa -n "__fish_seen_subcommand_from complete" -f -a "bash fish zsh powershell"
+complete -c maa -n "__fish_seen_subcommand_from mangen" -l path -r -d 'Directory to save man pages'
+complete -c maa -n "__fish_seen_subcommand_from cleanup" -f -a "cli-cache avatars log misc"
 complete -c maa -n "__fish_seen_subcommand_from convert" -f -s f -l format -a "j json y yaml t toml" -r
 complete -c maa -n "__fish_seen_subcommand_from activity" -f -a "$clients"
 complete -c maa -n "__fish_seen_subcommand_from remainder" -f -l timezone -d 'Timezone to determine the current date' -r
