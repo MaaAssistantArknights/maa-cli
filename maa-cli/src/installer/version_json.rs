519945cf598bf42fdffba0d3e053bb78cad6f663
Loong <wangl.cc@outlook.com>
2024-03-28 01:19:41 +0800

    ci: release alpha version as `alpha.n` or `beta.n.alpha.m` (#247)
    In current implementation, alpha version is released as `x.y.z+alpha.timestamp`. 
    Thus if user switches from beta to alpha channel, 
    the alpha version will be considered as outdated, 
    so the user will not be able to install the latest alpha version.

--- maa-cli/src/installer/version_json.rs
+++ maa-cli/src/installer/version_json.rs
@@ -7,17 +7,17 @@ pub struct VersionJSON<D> {
     details: D,
 }
 
-#[derive(Deserialize)]
-struct VersionJSONHelper<D> {
-    version: String,
-    details: D,
-}
-
 impl<'de, A: Deserialize<'de>> Deserialize<'de> for VersionJSON<A> {
     fn deserialize<D>(deserializer: D) -> Result<VersionJSON<A>, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
+        #[derive(Deserialize)]
+        struct VersionJSONHelper<D> {
+            version: String,
+            details: D,
+        }
+
         let helper = VersionJSONHelper::deserialize(deserializer)?;
         let version = if helper.version.starts_with('v') {
             Version::parse(&helper.version[1..])
@@ -54,5 +54,50 @@ impl<D> VersionJSON<D> {
 
     pub fn details(&self) -> &D {
         &self.details
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_can_update() {
+        fn can_update(remote: &str, current: &str, expected: bool) {
+            let version_json = VersionJSON {
+                version: Version::parse(remote).unwrap(),
+                details: (),
+            };
+
+            let current_version = Version::parse(current).unwrap();
+            assert_eq!(
+                version_json.can_update("test", &current_version).unwrap(),
+                expected
+            );
+        }
+
+        can_update("0.1.0", "0.0.9", true);
+        can_update("0.1.0", "0.1.0", false);
+        can_update("0.1.0", "0.1.1", false);
+
+        can_update("0.1.0", "0.1.0-beta", true);
+        can_update("0.1.0", "0.1.1-beta", false);
+
+        can_update("0.1.0", "0.1.0-alpha", true);
+        can_update("0.1.0", "0.1.1-alpha", false);
+
+        can_update("0.1.0-beta", "0.1.0", false);
+        can_update("0.1.1-beta", "0.1.0", true);
+
+        can_update("0.1.0-beta.1", "0.1.0-beta", true);
+        can_update("0.1.0-beta.2", "0.1.0-beta.1", true);
+
+        can_update("0.1.0-beta", "0.1.0-alpha", true);
+        can_update("0.1.0-beta", "0.1.0-alpha.1", true);
+        can_update("0.1.0-beta.1.alpha", "0.1.0-beta.1", true);
+        can_update("0.1.0-beta.1.alpha.1", "0.1.0-beta.1", true);
+        can_update("0.1.0-beta.1.alpha.2", "0.1.0-beta.1.alpha.1", true);
+        can_update("0.1.0-beta.2.alpha.1", "0.1.0-beta.1.alpha.2", true);
+        can_update("0.1.0-alpha.1+sha.1da7b3d", "0.1.0-alpha.1", true);
     }
 }
