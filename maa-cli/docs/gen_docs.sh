30594600676a40e35b9dddea391779b63719a0bf
Loong <wangl.cc@outlook.com>
2024-04-03 18:07:19 +0800

    chore(docs): fix relative links in generated docs

--- maa-cli/docs/gen_docs.sh
+++ maa-cli/docs/gen_docs.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # generate documentation used in main repo of MAA
 
-# output to the current directory
+# output to the specified directory
 output_dir=$1
 # the original directory of docs is at the same directory as this script
 original_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+
+files=(
+  intro.md
+  install.md
+  usage.md
+  config.md
+  faq.md
+)
 
 icons=(
   material-symbols:summarize
@@ -18,12 +27,13 @@ icons=(
 for lang in "$original_dir"/zh-CN; do
   echo "Generating documentation for $lang"
   order=0
-  for file in "$lang"/{intro,install,usage,config,faq}.md; do
-    echo "-> Generating documentation for $file"
+  for filename in "${files[@]}"; do
+    echo "-> Generating documentation for $filename"
+    file="$lang/$filename"
 
     index=$order
     order=$((order+1))
-    out_file="$output_dir/cli-$(basename "$file")"
+    out_file="$output_dir/cli-$filename"
     # insert metadata of markdown file to the beginning of the file
     {
       echo "---"
@@ -31,11 +41,13 @@ for lang in "$original_dir"/zh-CN; do
       echo "icon: ${icons[$index]}"
       echo "---"
       echo
+      cat "$file"
     } > "$out_file"
     # remap some links to the original repo
-    sed 's|\.\./\.\./|https://github.com/MaaAssistantArknights/maa-cli/blob/main/maa-cli/|g' < "$file" |
-      sed 's|https://maa\.plus/docs/|../../|g' >> "$out_file"
-
-    echo "-> Done"
+    sed -I '' -E 's|\.\./\.\./|https://github.com/MaaAssistantArknights/maa-cli/blob/main/maa-cli/|g' "$out_file"
+    sed -I '' -E 's|https://maa\.plus/docs/|../../|g' "$out_file"
+    for filename_md in "${files[@]}"; do
+      sed -i '' -E "s|$filename_md|cli-$filename_md|g" "$out_file"
+    done
   done
 done
