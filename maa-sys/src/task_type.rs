2417b788caf56c4c15181858b95c85ee82f55a72
Loong <wangl.cc@outlook.com>
2024-04-11 15:23:21 +0800

    feat: impl `FromStr` for `TaskType`

--- maa-sys/src/task_type.rs
+++ maa-sys/src/task_type.rs
@@ -39,6 +39,30 @@ const TASK_TYPE_STRS: [&str; 16] = [
     "VideoRecognition",
 ];
 
+#[cfg_attr(test, derive(PartialEq, Eq))]
+#[derive(Debug)]
+pub struct UnknownTaskType(String);
+
+impl std::fmt::Display for UnknownTaskType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "unknown task type `{}`", self.0)
+    }
+}
+
+impl std::error::Error for UnknownTaskType {}
+
+impl std::str::FromStr for TaskType {
+    type Err = UnknownTaskType;
+
+    fn from_str(s: &str) -> Result<TaskType, Self::Err> {
+        TASK_TYPE_STRS
+            .iter()
+            .position(|&x| x.eq_ignore_ascii_case(s))
+            .map(|i| unsafe { std::mem::transmute(i as u8) })
+            .ok_or_else(|| UnknownTaskType(s.to_owned()))
+    }
+}
+
 #[cfg(feature = "serde")]
 impl<'de> serde::Deserialize<'de> for TaskType {
     fn deserialize<D>(deserializer: D) -> Result<TaskType, D::Error>
@@ -58,26 +82,7 @@ impl<'de> serde::Deserialize<'de> for TaskType {
             where
                 E: serde::de::Error,
             {
-                use TaskType::*;
-                match value {
-                    "StartUp" | "startup" | "Startup" => Ok(StartUp),
-                    "CloseDown" | "closedown" | "Closedown" => Ok(CloseDown),
-                    "Fight" | "fight" => Ok(Fight),
-                    "Recruit" | "recruit" => Ok(Recruit),
-                    "Infrast" | "infrast" => Ok(Infrast),
-                    "Mall" | "mall" => Ok(Mall),
-                    "Award" | "award" => Ok(Award),
-                    "Roguelike" | "roguelike" => Ok(Roguelike),
-                    "Copilot" | "copilot" => Ok(Copilot),
-                    "SSSCopilot" | "ssscopilot" => Ok(SSSCopilot),
-                    "Depot" | "depot" => Ok(Depot),
-                    "OperBox" | "operbox" => Ok(OperBox),
-                    "ReclamationAlgorithm" | "reclamationalgorithm" => Ok(ReclamationAlgorithm),
-                    "Custom" | "custom" => Ok(Custom),
-                    "SingleStep" | "singlestep" => Ok(SingleStep),
-                    "VideoRecognition" | "videorecognition" => Ok(VideoRecognition),
-                    _ => Err(E::unknown_variant(value, &TASK_TYPE_STRS)),
-                }
+                value.parse().map_err(E::custom)
             }
         }
 
@@ -87,7 +92,7 @@ impl<'de> serde::Deserialize<'de> for TaskType {
 
 impl TaskType {
     fn to_str(self) -> &'static str {
-        TASK_TYPE_STRS[self as usize]
+        unsafe { TASK_TYPE_STRS.get_unchecked(self as usize) }
     }
 }
 
@@ -115,6 +120,38 @@ mod tests {
 
     use TaskType::*;
 
+    #[test]
+    fn parse() {
+        assert_eq!("StartUp".parse(), Ok(StartUp));
+        assert_eq!("CloseDown".parse(), Ok(CloseDown));
+        assert_eq!("Fight".parse(), Ok(Fight));
+        assert_eq!("Recruit".parse(), Ok(Recruit));
+        assert_eq!("Infrast".parse(), Ok(Infrast));
+        assert_eq!("Mall".parse(), Ok(Mall));
+        assert_eq!("Award".parse(), Ok(Award));
+        assert_eq!("Roguelike".parse(), Ok(Roguelike));
+        assert_eq!("Copilot".parse(), Ok(Copilot));
+        assert_eq!("SSSCopilot".parse(), Ok(SSSCopilot));
+        assert_eq!("Depot".parse(), Ok(Depot));
+        assert_eq!("OperBox".parse(), Ok(OperBox));
+        assert_eq!("ReclamationAlgorithm".parse(), Ok(ReclamationAlgorithm));
+        assert_eq!("Custom".parse(), Ok(Custom));
+        assert_eq!("SingleStep".parse(), Ok(SingleStep));
+        assert_eq!("VideoRecognition".parse(), Ok(VideoRecognition));
+        assert_eq!(
+            "Unknown".parse::<TaskType>(),
+            Err(UnknownTaskType("Unknown".to_owned()))
+        );
+    }
+
+    #[test]
+    fn error() {
+        assert_eq!(
+            UnknownTaskType("Unknown".to_owned()).to_string(),
+            "unknown task type `Unknown`",
+        );
+    }
+
     #[cfg(feature = "serde")]
     mod serde {
         use super::*;
@@ -123,45 +160,14 @@ mod tests {
 
         #[test]
         fn deserialize() {
-            let types: [TaskType; 16] = [
-                StartUp,
-                CloseDown,
-                Fight,
-                Recruit,
-                Infrast,
-                Mall,
-                Award,
-                Roguelike,
-                Copilot,
-                SSSCopilot,
-                Depot,
-                OperBox,
-                ReclamationAlgorithm,
-                Custom,
-                SingleStep,
-                VideoRecognition,
-            ];
+            let types: [TaskType; 2] = [StartUp, CloseDown];
 
             assert_de_tokens(
                 &types,
                 &[
-                    Token::Seq { len: Some(16) },
+                    Token::Seq { len: Some(2) },
                     Token::Str("StartUp"),
                     Token::Str("CloseDown"),
-                    Token::Str("Fight"),
-                    Token::Str("Recruit"),
-                    Token::Str("Infrast"),
-                    Token::Str("Mall"),
-                    Token::Str("Award"),
-                    Token::Str("Roguelike"),
-                    Token::Str("Copilot"),
-                    Token::Str("SSSCopilot"),
-                    Token::Str("Depot"),
-                    Token::Str("OperBox"),
-                    Token::Str("ReclamationAlgorithm"),
-                    Token::Str("Custom"),
-                    Token::Str("SingleStep"),
-                    Token::Str("VideoRecognition"),
                     Token::SeqEnd,
                 ],
             );
@@ -212,6 +218,7 @@ mod tests {
     #[test]
     fn to_cstring() {
         use crate::ToCString;
+
         use std::ffi::CString;
 
         assert_eq!(
