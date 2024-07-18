1ad9de09e84ab54ffecd44cb5dbb18ed609d3bf9
Loong <wangl.cc@outlook.com>
2024-07-18 10:18:24 +0100

    chore: add allow(dead_code) to `InitializedTaskConfig` fields (#294)

--- maa-cli/src/config/task/mod.rs
+++ maa-cli/src/config/task/mod.rs
@@ -286,7 +286,9 @@ impl super::FromFile for TaskConfig {}
 #[cfg_attr(test, derive(PartialEq, Debug))]
 pub struct InitializedTaskConfig {
     pub client_type: Option<ClientType>,
+    #[allow(dead_code)]
     pub start_app: bool,
+    #[allow(dead_code)]
     pub close_app: bool,
     pub tasks: Vec<InitializedTask>,
 }
