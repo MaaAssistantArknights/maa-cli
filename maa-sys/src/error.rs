212b43fe776925ca3c1fa6b9b904bca7d254437d
Loong <wangl.cc@outlook.com>
2024-08-07 23:52:50 +0100

    feat: improve error message for MaaError

--- maa-sys/src/error.rs
+++ maa-sys/src/error.rs
@@ -18,7 +18,7 @@ pub enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Error::MAAError => f.write_str("MAAError"),
+            Error::MAAError => f.write_str("MaaCore returned an error, check its log for details"),
             Error::BufferTooSmall => f.write_str("Buffer Too Small"),
             Error::NulError(err) => write!(f, "{}", err),
             Error::Utf8Error(Some(err)) => write!(f, "{}", err),
