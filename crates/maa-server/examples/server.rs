7f093af3be4b5b6971136bedda12e330186ba428
Jackhr-arch <63526062+Jackhr-arch@users.noreply.github.com>
2025-03-08 14:22:28 +0800

    catch ctrlc and other singals

--- crates/maa-server/examples/server.rs
+++ crates/maa-server/examples/server.rs
@@ -17,7 +17,6 @@ async fn main() {
 
     let parent_cancel_token = CancellationToken::new();
     let cancel_token = parent_cancel_token.child_token();
-    ctrlc::set_handler(move || parent_cancel_token.cancel()).unwrap();
 
     let timeout = std::time::Duration::from_micros(100);
 
@@ -45,8 +44,8 @@ async fn main() {
                 let token = child_cancel_token.child_token();
                 token.cancelled().await
             }) => {}
-            // make sure connection is closed
-            _ = child_cancel_token.cancelled() => {tokio::time::sleep(timeout).await}
+            _ = child_cancel_token.cancelled() => {}
+            _ = wait_for_signal() => {parent_cancel_token.cancel()}
         );
     } else {
         println!("Using Http Port");
@@ -60,13 +59,57 @@ async fn main() {
                 let token = child_cancel_token.child_token();
                 token.cancelled().await
             }) => {}
-            _ = child_cancel_token.cancelled() => {tokio::time::sleep(timeout).await}
+            _ = child_cancel_token.cancelled() => {}
+            _ = wait_for_signal() => {parent_cancel_token.cancel()}
         );
     }
-
+    // make sure connection is closed
+    tokio::time::sleep(timeout).await;
     println!("Exiting");
     if maa_sys::binding::loaded() {
         println!("Clean Up");
         maa_sys::binding::unload();
     }
+}
+
+/// Waits for a signal that requests a graceful shutdown, like SIGTERM or SIGINT.
+#[cfg(unix)]
+async fn wait_for_signal_impl() {
+    use tokio::signal::unix::{signal, SignalKind};
+
+    // Infos here:
+    // https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+    let mut signal_terminate = signal(SignalKind::terminate()).unwrap();
+    let mut signal_interrupt = signal(SignalKind::interrupt()).unwrap();
+
+    tokio::select! {
+        _ = signal_terminate.recv() => tracing::debug!("Received SIGTERM."),
+        _ = signal_interrupt.recv() => tracing::debug!("Received SIGINT."),
+    };
+}
+
+/// Waits for a signal that requests a graceful shutdown, Ctrl-C (SIGINT).
+#[cfg(windows)]
+async fn wait_for_signal_impl() {
+    use tokio::signal::windows;
+
+    // Infos here:
+    // https://learn.microsoft.com/en-us/windows/console/handlerroutine
+    let mut signal_c = windows::ctrl_c().unwrap();
+    let mut signal_break = windows::ctrl_break().unwrap();
+    let mut signal_close = windows::ctrl_close().unwrap();
+    let mut signal_shutdown = windows::ctrl_shutdown().unwrap();
+
+    tokio::select! {
+        _ = signal_c.recv() => tracing::debug!("Received CTRL_C."),
+        _ = signal_break.recv() => tracing::debug!("Received CTRL_BREAK."),
+        _ = signal_close.recv() => tracing::debug!("Received CTRL_CLOSE."),
+        _ = signal_shutdown.recv() => tracing::debug!("Received CTRL_SHUTDOWN."),
+    };
+}
+
+/// Registers signal handlers and waits for a signal that
+/// indicates a shutdown request.
+pub(crate) async fn wait_for_signal() {
+    wait_for_signal_impl().await
 }
