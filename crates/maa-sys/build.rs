4da609ec20038db6fe0eb4c055e7d17008896389
Loong <git@loongw.cc>
2025-07-06 10:32:14 +0800

    fix

--- crates/maa-sys/build.rs
+++ crates/maa-sys/build.rs
@@ -11,7 +11,7 @@ fn dynamic_link() {
     let core_dir = var_os("MAA_CORE_DIR")
         .map(PathBuf::from)
         .expect("MAA_CORE_DIR not set");
-    let core_name = format!("{}MaaCore{}", DLL_PREFIX, DLL_SUFFIX);
+    let core_name = format!("{DLL_PREFIX}MaaCore{DLL_SUFFIX}");
     if !core_dir.join(core_name).exists() {
         panic!("cannot find maa core, make sure you have installed maa core at correct path");
     }
