5ec09a71776c500207aa6794f4e4bef3db7e899c
PFiS1737 <pfis1737@gmail.com>
2026-05-23 23:42:11 +0800

    format

--- crates/maa-cli/src/command.rs
+++ crates/maa-cli/src/command.rs
@@ -323,13 +323,10 @@ mod test {
     #[cfg(feature = "core_installer")]
     #[test]
     fn install() {
-        assert_matches!(
-            parse_from(["maa", "install"]).command,
-            Command::Install {
-                common: config::cli::maa_core::CommonArgs { .. },
-                force: false,
-            }
-        );
+        assert_matches!(parse_from(["maa", "install"]).command, Command::Install {
+            common: config::cli::maa_core::CommonArgs { .. },
+            force: false,
+        });
 
         assert_matches!(
             parse_from(["maa", "install", "beta"]).command,
@@ -395,12 +392,9 @@ mod test {
     #[cfg(feature = "core_installer")]
     #[test]
     fn update() {
-        assert_matches!(
-            parse_from(["maa", "update"]).command,
-            Command::Update {
-                common: config::cli::maa_core::CommonArgs { .. },
-            }
-        );
+        assert_matches!(parse_from(["maa", "update"]).command, Command::Update {
+            common: config::cli::maa_core::CommonArgs { .. },
+        });
     }
 
     #[cfg(feature = "cli_installer")]
@@ -436,26 +430,22 @@ mod test {
 
     #[test]
     fn dir() {
-        assert_matches!(
-            parse_from(["maa", "dir", "data"]).command,
-            Command::Dir { dir: Dir::Data }
-        );
+        assert_matches!(parse_from(["maa", "dir", "data"]).command, Command::Dir {
+            dir: Dir::Data
+        });
         assert_matches!(
             parse_from(["maa", "dir", "library"]).command,
             Command::Dir { dir: Dir::Library }
         );
-        assert_matches!(
-            parse_from(["maa", "dir", "lib"]).command,
-            Command::Dir { dir: Dir::Library }
-        );
-        assert_matches!(
-            parse_from(["maa", "dir", "config"]).command,
-            Command::Dir { dir: Dir::Config }
-        );
-        assert_matches!(
-            parse_from(["maa", "dir", "cache"]).command,
-            Command::Dir { dir: Dir::Cache }
-        );
+        assert_matches!(parse_from(["maa", "dir", "lib"]).command, Command::Dir {
+            dir: Dir::Library
+        });
+        assert_matches!(parse_from(["maa", "dir", "config"]).command, Command::Dir {
+            dir: Dir::Config
+        });
+        assert_matches!(parse_from(["maa", "dir", "cache"]).command, Command::Dir {
+            dir: Dir::Cache
+        });
         assert_matches!(
             parse_from(["maa", "dir", "resource"]).command,
             Command::Dir { dir: Dir::Resource }
@@ -466,20 +456,16 @@ mod test {
                 dir: Dir::HotUpdate
             }
         );
-        assert_matches!(
-            parse_from(["maa", "dir", "log"]).command,
-            Command::Dir { dir: Dir::Log }
-        );
+        assert_matches!(parse_from(["maa", "dir", "log"]).command, Command::Dir {
+            dir: Dir::Log
+        });
     }
 
     #[test]
     fn version() {
-        assert_matches!(
-            parse_from(["maa", "version"]).command,
-            Command::Version {
-                component: Component::All
-            }
-        );
+        assert_matches!(parse_from(["maa", "version"]).command, Command::Version {
+            component: Component::All
+        });
         assert_matches!(
             parse_from(["maa", "version", "all"]).command,
             Command::Version {
@@ -597,12 +583,9 @@ mod test {
 
     #[test]
     fn activity() {
-        assert_matches!(
-            parse_from(["maa", "activity"]).command,
-            Command::Activity {
-                client: config::task::ClientType::Official,
-            }
-        );
+        assert_matches!(parse_from(["maa", "activity"]).command, Command::Activity {
+            client: config::task::ClientType::Official,
+        });
 
         assert_matches!(
             parse_from(["maa", "activity", "YoStarEN"]).command,
@@ -700,14 +683,11 @@ mod test {
 
     #[test]
     fn init() {
-        assert_matches!(
-            parse_from(["maa", "init"]).command,
-            Command::Init {
-                name: None,
-                format: None,
-                force: false,
-            }
-        );
+        assert_matches!(parse_from(["maa", "init"]).command, Command::Init {
+            name: None,
+            format: None,
+            force: false,
+        });
 
         assert_matches!(
             parse_from(["maa", "init", "--name", "name"]).command,
@@ -725,13 +705,10 @@ mod test {
             }
         );
 
-        assert_matches!(
-            parse_from(["maa", "init", "-ft"]).command,
-            Command::Init {
-                format: Some(config::Filetype::Toml),
-                ..
-            }
-        );
+        assert_matches!(parse_from(["maa", "init", "-ft"]).command, Command::Init {
+            format: Some(config::Filetype::Toml),
+            ..
+        });
 
         assert_matches!(
             parse_from(["maa", "init", "--force"]).command,
